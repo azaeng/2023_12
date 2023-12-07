cbd1b42266681da80688209d84e70da9b5b61700
azaeng <aa22548659@gmail.com>
2023-12-08 03:14:11 +0900

    2+4,3+4

--- a/a.c
+++ a/a.c
@@ -3,11 +3,11 @@
 int main()
 {
 // <<<<<<< HEAD
-    int result1 = 2 + 3;
+    int result1 = 2 + 4;
 
     printf("2 + 3 = %d\n", result1);
 // =======
-    int result2 = 3 + 3;
+    int result2 = 3 + 4;
 
     printf("3 + 3 = %d\n", result2);
 // >>>>>>> b
